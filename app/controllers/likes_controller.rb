0f29ef91a3394c6cd556194c5421bc6dcc9aa7fc
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-06 00:02:45 +0900

    Update コントローラ作成、バリデーションの設定など
    コントローラ編集
    バリデーション設定
    テーブル設定追加
    ルーティング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,14 @@
 class LikesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.build(post: @post)
+    if @like.save
+      redirect_to @post, notice: 'Liked!'
+    else
+      redirect_to @post, alert: 'Error'
   end
 
   def destroy
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_05_141943) do
+ActiveRecord::Schema.define(version: 2023_04_05_145523) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_04_05_141943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
