cfdae806c2483513253998b37b287bc96e8969f9
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-05 23:29:50 +0900

    Create Likeモデル, likesコントローラ
    初期作成, user.rb, post.rbのアソシエーション設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'posts#index'
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
