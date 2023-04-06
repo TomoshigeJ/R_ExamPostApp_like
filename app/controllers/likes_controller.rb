24657466ee1b915ae6813f00a3a3eaa4ec77aac7
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-06 10:07:28 +0900

    Update like解除
    コントローラ修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,10 +10,10 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like = current_user.likes.find_by(post_id: params[:post_id])
+    @like = current_user.likes.find_by(post_id: params[:id])
     if @like
       @like.destroy
-      redirect_to posts_path, succless: 'Unliked!'
+      redirect_to posts_path, success: 'Unliked!'
     else
       redirect_to posts_path, danger: 'Error'
     end
